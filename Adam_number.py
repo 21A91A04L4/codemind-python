efc1275097df78a55a13a3fcf23d4bdb2dcd72ec
KOMMIREDDY SUDHA <96324729+21A91A04L4@users.noreply.github.com>
2023-07-06 19:30:17 +0530

    def reverse(n):     rev=0     while n>0:         r=n%10         rev=rev*10+r         n=n//10     return rev n=int(input()) m=n*n k=reverse(n) x=k*k if m==reverse(x):     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,16 +1,15 @@
-def rev(n):
-    s=0
+def reverse(n):
+    rev=0
     while n>0:
         r=n%10
-        s=s*10+r
+        rev=rev*10+r
         n=n//10
-    return s
+    return rev
 n=int(input())
 m=n*n
-n1=rev(n)
-m1=n1*n1
-if(m==rev(m1)):
+k=reverse(n)
+x=k*k
+if m==reverse(x):
     print("True")
 else:
     print("False")
-    
