55cddf950a601500c60bdc9e6e223237fc4cf6b1
KOMMIREDDY SUDHA <96324729+21A91A04L4@users.noreply.github.com>
2023-07-08 12:04:29 +0530

    n=int(input()) l=list(map(int,input().split())) p=[] k=[] s=n/2 for i in range(len(l)):     if l[i]<= s:         p.append(l[i])     else:         k.append(l[i]) print(sum(p)) print(sum(k))

---
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+p=[]
+k=[]
+s=n/2
+for i in range(len(l)):
+    if l[i]<= s:
+        p.append(l[i])
+    else:
+        k.append(l[i])
+print(sum(p))
+print(sum(k))
+
+    
