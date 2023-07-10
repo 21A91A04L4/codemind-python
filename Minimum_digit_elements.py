19a80c401e84ee86c86930d82924ba4160f11390
KOMMIREDDY SUDHA <96324729+21A91A04L4@users.noreply.github.com>
2023-07-10 15:31:11 +0530

    def dig(n):     c=0     if(n==0):         return 1     if(n<0):         n=abs(n)     while n>0:         r=n%10
            c+=1
            n=n//10
        return c
            
    n=int(input())
    l=list(map(int,input().split()))
    j=[]
    for i in range(len(l)):
        x=dig(l[i])
        j.append(x)
    k=min(j)
    c=0
    for i in j:
        if(i==k):
            c+=1
    print(c)
    #k=0
    #if v in j:
     #   k+=1
    #print(k)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,30 @@
+def dig(n):
+    c=0
+    if(n==0):
+        return 1
+    if(n<0):
+        n=abs(n)
+    while n>0:
+        r=n%10
+        
+        
+        c+=1
+        n=n//10
+    return c
+        
+n=int(input())
+l=list(map(int,input().split()))
+j=[]
+for i in range(len(l)):
+    x=dig(l[i])
+    j.append(x)
+k=min(j)
+c=0
+for i in j:
+    if(i==k):
+        c+=1
+print(c)
+#k=0
+#if v in j:
+ #   k+=1
+#print(k)
